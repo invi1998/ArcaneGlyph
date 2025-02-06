43d3452fdf0b76b87c64ba102b9219da18d61eb1
invi1998 <2267661826@qq.com>
2025-02-06 19:44:24 +0800

    自定义增强输入组件并启用

--- Source/ArcaneGlyph/Public/Component/Input/ArcaneInputComponent.h
+++ Source/ArcaneGlyph/Public/Component/Input/ArcaneInputComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DadaAsset_InputConfig.h"
 #include "ArcaneInputComponent.generated.h"
 
 /**
@@ -13,5 +14,16 @@ UCLASS()
 class ARCANEGLYPH_API UArcaneInputComponent : public UEnhancedInputComponent
 {
 	GENERATED_BODY()
+
+public:
+	template<typename UserObject, typename CallbackFunction>
+	void BindNativeInputAction(const UDadaAsset_InputConfig* InInputConfig, const FGameplayTag& InTag, ETriggerEvent InEvent, UserObject* InContextObject, CallbackFunction InCallbackFunction)
+	{
+		checkf(InInputConfig, TEXT("InputConfig is nullptr, cannot bind native input action"));
+		if (UInputAction* InputAction = InInputConfig->GetNativeInputActionByTag(InTag))
+		{
+			BindAction(InputAction, InEvent, InContextObject, InCallbackFunction);
+		}
+	}
 	
 };
