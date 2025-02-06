f3d5ed551cb7af666138c956d3105182831af2f8
invi1998 <2267661826@qq.com>
2025-02-06 17:20:10 +0800

    创建GameplayTags

---
+++ Source/ArcaneGlyph/Private/ArcaneGameplayTags.cpp
@@ -0,0 +1,12 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "ArcaneGameplayTags.h"
+
+namespace ArcaneGameplayTags
+{
+	// Input Tags
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Move, "InputTag.Move", "角色移动");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Look, "InputTag.Look", "角色观察");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Jump, "InputTag.Jump", "角色跳跃");
+}
---
+++ Source/ArcaneGlyph/Public/ArcaneGameplayTags.h
@@ -0,0 +1,15 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ArcaneGameplayTags
+{
+	// Input Tags
+	ARCANEGLYPH_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	ARCANEGLYPH_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+	ARCANEGLYPH_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Jump);
+	
+}
+
