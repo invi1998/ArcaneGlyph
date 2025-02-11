05618ce08a72d30d728e557bc3ce9d3a05f90a5f
invi1998 <2267661826@qq.com>
2025-02-11 22:46:24 +0800

    AbilitySystem

---
+++ Source/ArcaneGlyph/Public/AbilitySystem/ArcaneAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "ArcaneAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANEGLYPH_API UArcaneAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
