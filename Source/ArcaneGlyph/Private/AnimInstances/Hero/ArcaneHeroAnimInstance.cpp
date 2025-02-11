a47c6a53b7c52893ea10884ae300f2b96df3ee29
invi1998 <2267661826@qq.com>
2025-02-11 22:19:44 +0800

    角色放松状态动画

--- Source/ArcaneGlyph/Private/AnimInstances/Hero/ArcaneHeroAnimInstance.cpp
+++ Source/ArcaneGlyph/Private/AnimInstances/Hero/ArcaneHeroAnimInstance.cpp
@@ -2,3 +2,39 @@
 
 
 #include "AnimInstances/Hero/ArcaneHeroAnimInstance.h"
+
+#include "Characters/ArcaneHeroCharacter.h"
+
+
+void UArcaneHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (IsValid(OwnerCharacter))
+	{
+		OwnerHeroCharacter = Cast<AArcaneHeroCharacter>(OwnerCharacter);
+	}
+}
+
+void UArcaneHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxedState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaSeconds;
+		if (IdleElapsedTime >= EnterRelaxedStateTime)
+		{
+			bShouldEnterRelaxedState = true;
+		}
+		else
+		{
+			bShouldEnterRelaxedState = false;
+		}
+	}
+}
--- Source/ArcaneGlyph/Public/AnimInstances/Hero/ArcaneHeroAnimInstance.h
+++ Source/ArcaneGlyph/Public/AnimInstances/Hero/ArcaneHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/ArcaneCharacterAnimInstance.h"
 #include "ArcaneHeroAnimInstance.generated.h"
 
+class AArcaneHeroCharacter;
 /**
  * 
  */
@@ -13,4 +14,21 @@ UCLASS()
 class ARCANEGLYPH_API UArcaneHeroAnimInstance : public UArcaneCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|ReferenceData")
+	TObjectPtr<AArcaneHeroCharacter> OwnerHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxedState;		// 是否应该进入放松状态
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxedStateTime = 5.f;		// 进入放松状态的时间
+
+	float IdleElapsedTime = 0.f;		// 空闲经过的时间
+	
 };
