e2cdd767d90e95ce1023f23022a62c3c18dbef28
invi1998 <2267661826@qq.com>
2025-02-15 14:16:34 +0800

    自定义蓝图函数库

---
+++ Source/ArcaneGlyph/Private/ArcaneBlueprintFunctionLibrary.cpp
@@ -0,0 +1,53 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "ArcaneBlueprintFunctionLibrary.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/ArcaneAbilitySystemComponent.h"
+
+
+UArcaneAbilitySystemComponent* UArcaneBlueprintFunctionLibrary::NativeGetArcaneASCFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	// 只要InActor实现了 IAbilitySystemInterface 接口的 GetAbilitySystemComponent函数，就可以获取到AbilitySystemComponent
+	return CastChecked<UArcaneAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+void UArcaneBlueprintFunctionLibrary::AddGameplayTagToActorIfNotHas(AActor* InActor, const FGameplayTag& InTag)
+{
+	if (UArcaneAbilitySystemComponent* ArcaneASC = NativeGetArcaneASCFromActor(InActor))
+	{
+		if (!ArcaneASC->HasMatchingGameplayTag(InTag))
+		{
+			ArcaneASC->AddLooseGameplayTag(InTag);
+		}
+	}
+}
+
+void UArcaneBlueprintFunctionLibrary::RemoveGameplayTagFromActorIfHas(AActor* InActor, const FGameplayTag& InTag)
+{
+	if (UArcaneAbilitySystemComponent* ArcaneASC = NativeGetArcaneASCFromActor(InActor))
+	{
+		if (ArcaneASC->HasMatchingGameplayTag(InTag))
+		{
+			ArcaneASC->RemoveLooseGameplayTag(InTag);
+		}
+	}
+}
+
+bool UArcaneBlueprintFunctionLibrary::NativeDoesActorHasGameplayTag(AActor* InActor, const FGameplayTag& InTag)
+{
+	if (UArcaneAbilitySystemComponent* ArcaneASC = NativeGetArcaneASCFromActor(InActor))
+	{
+		return ArcaneASC->HasMatchingGameplayTag(InTag);
+	}
+	return false;
+}
+
+void UArcaneBlueprintFunctionLibrary::BP_DoesActorHasGameplayTag(AActor* InActor, const FGameplayTag& InTag, EArcaneConfirmType& ConfirmType)
+{
+	// 通过调用原生函数来判断Actor是否拥有指定的GameplayTag,并将结果赋值给ConfirmType
+	ConfirmType = NativeDoesActorHasGameplayTag(InActor, InTag) ? EArcaneConfirmType::YES : EArcaneConfirmType::NO;
+}
