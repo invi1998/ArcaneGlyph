ae496abdfd5eb373789deaa62b101edc4fce7586
invi1998 <2267661826@qq.com>
2025-02-19 11:29:00 +0800

    属性集设定

--- Source/ArcaneGlyph/Public/AbilitySystem/ArcaneAttributeSet.h
+++ Source/ArcaneGlyph/Public/AbilitySystem/ArcaneAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/ArcaneAbilitySystemComponent.h"
 #include "ArcaneAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,4 +20,34 @@ UCLASS()
 class ARCANEGLYPH_API UArcaneAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UArcaneAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, CurrentHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, CurrentRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, MaxRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, AttackPower);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UArcaneAttributeSet, DefensePower);
+
+protected:
+	
 };
