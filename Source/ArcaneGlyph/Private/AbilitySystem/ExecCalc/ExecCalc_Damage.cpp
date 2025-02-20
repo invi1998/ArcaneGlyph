95b22eabd4a443a3049837c1115d73ec77e1808a
invi1998 <2267661826@qq.com>
2025-02-20 14:01:47 +0800

    两种捕获角色属性的方式

--- Source/ArcaneGlyph/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/ArcaneGlyph/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,3 +3,26 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	// 捕获到伤害属性
+	// 有两种方式可以捕获属性
+	// 1: 较慢的方式，通过属性名称来查找属性
+	/*
+	FProperty* AttackPowerProperty = FindFieldChecked<FProperty>(
+		UArcaneAttributeSet::StaticClass(),
+		GET_MEMBER_NAME_CHECKED(UArcaneAttributeSet, AttackPower)
+	);
+	FGameplayEffectAttributeCaptureDefinition AttackPowerDef(
+		AttackPowerProperty,
+		EGameplayEffectAttributeCaptureSource::Source,
+		false
+	);
+	RelevantAttributesToCapture.Add(AttackPowerDef);
+	*/
+
+	// 2: 更快的方式，通过属性标签来查找属性
+	RelevantAttributesToCapture.Add(GetArcaneDamageCaptureStatics().AttackPowerDef);	// 捕获攻击力属性
+	RelevantAttributesToCapture.Add(GetArcaneDamageCaptureStatics().DefensePowerDef);	// 捕获防御力属性
+	
+}
--- Source/ArcaneGlyph/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
+++ Source/ArcaneGlyph/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -4,7 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "GameplayEffectExecutionCalculation.h"
+#include "AbilitySystem/ArcaneAttributeSet.h"
 #include "ExecCalc_Damage.generated.h"
+
+struct ArcaneDamageCaptureStatics
+{
+	// 该宏用于声明一个捕获属性的结构体，其中包含了捕获属性的指针，以及捕获属性的标签
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower);
+
+	ArcaneDamageCaptureStatics()
+	{
+		// 通过捕获属性的标签来捕获属性
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UArcaneAttributeSet, AttackPower, Source, false);	// 捕获源目标的攻击力，不拍快照
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UArcaneAttributeSet, DefensePower, Target, false);	// 捕获目标的防御力，不拍快照
+	}
+};
+
+// 该静态函数将在每次调用时返回一个ArcaneDamageStatics的静态实例，以确保我们不会在每次调用时创建一个新的实例
+static const ArcaneDamageCaptureStatics& GetArcaneDamageCaptureStatics()
+{
+	static ArcaneDamageCaptureStatics DStatics;
+	return DStatics;
+}
 
 /**
  * 
@@ -13,5 +35,9 @@ UCLASS()
 class ARCANEGLYPH_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
 {
 	GENERATED_BODY()
+
+public:
+	UExecCalc_Damage();
+	
 	
 };
