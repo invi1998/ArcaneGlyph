a5cbb86c659a3f145641f22a335f2bf74c4f1c5f
invi1998 <2267661826@qq.com>
2025-03-03 00:24:01 +0800

    火球音效

--- Source/ArcaneGlyph/Private/Items/ArcaneProjectileBase.cpp
+++ Source/ArcaneGlyph/Private/Items/ArcaneProjectileBase.cpp
@@ -3,6 +3,9 @@
 
 #include "Items/ArcaneProjectileBase.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "ArcaneBlueprintFunctionLibrary.h"
+#include "ArcaneGameplayTags.h"
 #include "Components/BoxComponent.h"
 #include "NiagaraComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
@@ -52,6 +55,44 @@ void AArcaneProjectileBase::BeginPlay()
 
 void AArcaneProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
+	BP_OnSpawnProjectileHitFX(Hit.ImpactPoint);
+	APawn* HitPawn = Cast<APawn>(OtherActor);
+	
+	// 如果目标不是敌对的，或者目标不是Pawn，就不做任何事情
+	// || UArcaneBlueprintFunctionLibrary::IsTargetPawnHostile(GetInstigator<APawn>(), HitPawn)
+	if (!HitPawn)
+	{
+		Destroy();
+		return;
+	}
+
+	bool bIsValidBlock = false;
+	const bool bIsPlayerBlocking = UArcaneBlueprintFunctionLibrary::NativeDoesActorHasGameplayTag(HitPawn, ArcaneGameplayTags::Player_Status_Blocking);
+
+	if (bIsPlayerBlocking)
+	{
+		bIsValidBlock = UArcaneBlueprintFunctionLibrary::IsCurrentBlockValid(GetInstigator<APawn>(), HitPawn);
+	}
+
+	FGameplayEventData EventData;
+	EventData.Target = HitPawn;
+	EventData.Instigator = this;
+
+	if (bIsValidBlock)
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			HitPawn,
+			ArcaneGameplayTags::Player_Event_BlockSuccess,
+			EventData
+		);
+	}
+	else
+	{
+		// apply damage
+	}
+
+	Destroy();
+	
 }
 
 void AArcaneProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
