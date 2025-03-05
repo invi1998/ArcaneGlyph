559bbe1925786137639475bc6334c9926b54be4e
invi1998 <2267661826@qq.com>
2025-03-05 20:49:52 +0800

    UAbilityTask_WaitSpawnEnemies

---
+++ Source/ArcaneGlyph/Private/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.cpp
@@ -0,0 +1,4 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h"
---
+++ Source/ArcaneGlyph/Public/AbilitySystem/AbilityTasks/AbilityTask_WaitSpawnEnemies.h
@@ -0,0 +1,16 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_WaitSpawnEnemies.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANEGLYPH_API UAbilityTask_WaitSpawnEnemies : public UAbilityTask
+{
+	GENERATED_BODY()
+};
