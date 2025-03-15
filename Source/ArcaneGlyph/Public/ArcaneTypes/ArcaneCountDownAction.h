e522d08e82f515c673eae70f51f4b63b114e74d7
invi1998 <2267661826@qq.com>
2025-03-15 15:34:22 +0800

    倒计时动作类

---
+++ Source/ArcaneGlyph/Public/ArcaneTypes/ArcaneCountDownAction.h
@@ -0,0 +1,38 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArcaneEnumTypes.h"
+
+class FArcaneCountDownAction : public FPendingLatentAction
+{
+public:
+	FArcaneCountDownAction(float InTotalCountDownTime, float InUpdateInterval, float& InOutRemainingTime, EArcaneCountDownActionOutput& InCountDownActionOutput, const FLatentActionInfo& LatentInfo)
+		: bNeedToCancel(false)
+		, TotalCountDownTime(InTotalCountDownTime)
+		, UpdateInterval(InUpdateInterval)
+		, OutRemainingTime(InOutRemainingTime)
+		, CountDownActionOutput(InCountDownActionOutput)
+		, ExecutionFunction(LatentInfo.ExecutionFunction)
+		, OutputLink(LatentInfo.Linkage)
+		, CallbackTarget(LatentInfo.CallbackTarget)
+		, ElapsedIntervalTime(0.f)
+		, ElapsedTimeSinceStart(0.f)
+	{
+	}
+
+private:
+	bool bNeedToCancel;	// 是否需要取消
+	float TotalCountDownTime;	// 总倒计时时间
+	float UpdateInterval;		// 更新间隔
+	
+	float& OutRemainingTime;		// 剩余时间
+
+	EArcaneCountDownActionOutput& CountDownActionOutput;
+	FName ExecutionFunction;
+	int32 OutputLink;
+	FWeakObjectPtr CallbackTarget;
+	float ElapsedIntervalTime;	// 已经过去的时间
+	float ElapsedTimeSinceStart;	// 从开始到现在经过的时间
+};
