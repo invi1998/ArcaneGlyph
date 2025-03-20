2d23e731c837450b4c18d9bc5013dcfb934a9f95
invi1998 <2267661826@qq.com>
2025-03-20 13:37:00 +0800

    BP Stone

--- Source/ArcaneGlyph/Private/Items/PickUp/ArcaneStoneBase.cpp
+++ Source/ArcaneGlyph/Private/Items/PickUp/ArcaneStoneBase.cpp
@@ -8,19 +8,6 @@
 AArcaneStoneBase::AArcaneStoneBase()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void AArcaneStoneBase::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AArcaneStoneBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	PrimaryActorTick.bCanEverTick = false;
 }
 
