aa9dd65fbaf8b1d2e38964191f45a474f2a34ee2
invi1998 <2267661826@qq.com>
2025-06-03 19:53:18 +0800

    生存游戏模式类

---
+++ Source/ArcaneGlyph/Private/Game/ArcaneSurvialGameModeBase.cpp
@@ -0,0 +1,21 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "Game/ArcaneSurvialGameModeBase.h"
+
+void AArcaneSurvialGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AArcaneSurvialGameModeBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AArcaneSurvialGameModeBase::SetCurrentSurvialState(EArcaneSurvialGameModeState InState)
+{
+	CurrentSurvialGameModeState = InState;
+
+	OnSurvialGameModeStateChanged.Broadcast(CurrentSurvialGameModeState);
+}
---
+++ Source/ArcaneGlyph/Public/Game/ArcaneSurvialGameModeBase.h
@@ -0,0 +1,44 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArcaneGameModeBase.h"
+#include "ArcaneSurvialGameModeBase.generated.h"
+
+UENUM(BlueprintType)
+enum class EArcaneSurvialGameModeState : uint8
+{
+	WaitSpawnNewWave UMETA(DisplayName = "等待生成新波次"),
+	SpawningNewWave UMETA(DisplayName = "生成新波次中"),
+	WaveInProgress UMETA(DisplayName = "波次进行中"),
+	WaveCompleted UMETA(DisplayName = "波次完成"),
+	AllWavesCompleted UMETA(DisplayName = "所有波次完成"),
+	PlayerDied UMETA(DisplayName = "玩家死亡"),
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvialGameModeStateChangedDelegate, EArcaneSurvialGameModeState, state);
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANEGLYPH_API AArcaneSurvialGameModeBase : public AArcaneGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+
+private:
+	void SetCurrentSurvialState(EArcaneSurvialGameModeState InState);
+	
+	UPROPERTY()
+	EArcaneSurvialGameModeState CurrentSurvialGameModeState = EArcaneSurvialGameModeState::WaitSpawnNewWave;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnSurvialGameModeStateChangedDelegate OnSurvialGameModeStateChanged;
+
+};
