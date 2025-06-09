2f54e709475dd7c0ed7f64dc8518805a1881fb04
invi1998 <2267661826@qq.com>
2025-06-09 15:16:19 +0800

    Set Up Options Screen

---
+++ Source/ArcaneGlyph/Private/Widget/Options/Widget_OptionsBase.cpp
@@ -0,0 +1,4 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "Widget/Options/Widget_OptionsBase.h"
---
+++ Source/ArcaneGlyph/Public/Widget/Options/Widget_OptionsBase.h
@@ -0,0 +1,16 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widget/Widget_ActivatableBase.h"
+#include "Widget_OptionsBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta=(DisabledNativeTick))
+class ARCANEGLYPH_API UWidget_OptionsBase : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+};
