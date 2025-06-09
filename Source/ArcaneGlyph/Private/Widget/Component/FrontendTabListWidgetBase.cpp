9d20d9cfdf7754d53e8a4e27abe79b482591f62f
invi1998 <2267661826@qq.com>
2025-06-10 01:42:46 +0800

    Common Tab List Widget

---
+++ Source/ArcaneGlyph/Private/Widget/Component/FrontendTabListWidgetBase.cpp
@@ -0,0 +1,22 @@
+// INVI_1998 All Rights Reserved.
+
+
+#include "Widget/Component/FrontendTabListWidgetBase.h"
+
+#include "Editor/WidgetCompilerLog.h"
+#include "Widget/Component/FrontendCommonButtonBase.h"
+
+#if WITH_EDITOR
+void UFrontendTabListWidgetBase::ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!TabButtonEntryWidgetClass)
+	{
+		CompileLog.Error(FText::FromString(
+			TEXT("The variable TabButtonEntryWidgetClass has no valid entry specified. ") +
+			GetClass()->GetName() + TEXT(" requires a valid widget class to be set for tab buttons.")
+			));
+	}
+}
+#endif
---
+++ Source/ArcaneGlyph/Public/Widget/Component/FrontendTabListWidgetBase.h
@@ -0,0 +1,30 @@
+// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "FrontendTabListWidgetBase.generated.h"
+
+class UFrontendCommonButtonBase;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta=(DisabledNativeTick))
+class ARCANEGLYPH_API UFrontendTabListWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+
+private:
+	// ~ Begin UWidget interface
+#if WITH_EDITOR
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+	// ~ End UWidget interface
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Tab List Widget", meta = (AllowPrivateAccess = "true", ClampMin = "1", ClampMax = "10"))
+	int32 DebugEditorPreviewTabCount = 3;	// 用于调试编辑器预览标签的计数器
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Tab List Widget", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UFrontendCommonButtonBase> TabButtonEntryWidgetClass;	// 标签按钮条目的小部件类
+};
