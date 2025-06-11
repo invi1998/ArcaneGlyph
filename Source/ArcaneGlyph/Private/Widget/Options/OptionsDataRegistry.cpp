461545e224c652d4f99dd649e8f56cd65d4dfb5d
invi1998 <2267661826@qq.com>
2025-06-12 01:44:51 +0800

    选项数据注册表

---
+++ Source/ArcaneGlyph/Private/Widget/Options/OptionsDataRegistry.cpp
@@ -0,0 +1,50 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "Widget/Options/OptionsDataRegistry.h"
+
+#include "Widget/Options/DataObject/ListDataObject_Collection.h"
+
+void UOptionsDataRegistry::InitOptionsDataRegistry(ULocalPlayer* InOwningLocalPlayer)
+{
+	InitGameplayCollectionTab();
+	InitControlsCollectionTab();
+	InitAudioCollectionTab();
+	InitVideoCollectionTab();
+}
+
+void UOptionsDataRegistry::InitGameplayCollectionTab()
+{
+	UListDataObject_Collection* GameplayCollectionDataObject = NewObject<UListDataObject_Collection>(this, UListDataObject_Collection::StaticClass());
+	GameplayCollectionDataObject->SetDataID(FName("GameplayTabCollection"));
+	GameplayCollectionDataObject->SetDisabledRichText(FText::FromString(TEXT("游戏")));
+
+	RegisteredOptionsTabCollections.Add(GameplayCollectionDataObject);
+}
+
+void UOptionsDataRegistry::InitControlsCollectionTab()
+{
+	UListDataObject_Collection* ControlsCollectionDataObject = NewObject<UListDataObject_Collection>(this, UListDataObject_Collection::StaticClass());
+	ControlsCollectionDataObject->SetDataID(FName("ControlsTabCollection"));
+	ControlsCollectionDataObject->SetDisabledRichText(FText::FromString(TEXT("控制器")));
+
+	RegisteredOptionsTabCollections.Add(ControlsCollectionDataObject);
+}
+
+void UOptionsDataRegistry::InitAudioCollectionTab()
+{
+	UListDataObject_Collection* AudioCollectionDataObject = NewObject<UListDataObject_Collection>(this, UListDataObject_Collection::StaticClass());
+	AudioCollectionDataObject->SetDataID(FName("AudioTabCollection"));
+	AudioCollectionDataObject->SetDisabledRichText(FText::FromString(TEXT("声音")));
+
+	RegisteredOptionsTabCollections.Add(AudioCollectionDataObject);
+}
+
+void UOptionsDataRegistry::InitVideoCollectionTab()
+{
+	UListDataObject_Collection* VideoCollectionDataObject = NewObject<UListDataObject_Collection>(this, UListDataObject_Collection::StaticClass());
+	VideoCollectionDataObject->SetDataID(FName("VideoTabCollection"));
+	VideoCollectionDataObject->SetDisabledRichText(FText::FromString(TEXT("显示")));
+
+	RegisteredOptionsTabCollections.Add(VideoCollectionDataObject);
+}
---
+++ Source/ArcaneGlyph/Public/Widget/Options/OptionsDataRegistry.h
@@ -0,0 +1,34 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "OptionsDataRegistry.generated.h"
+
+class UListDataObject_Collection;
+/**
+ * 
+ */
+UCLASS()
+class ARCANEGLYPH_API UOptionsDataRegistry : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// 这个函数将由 选项设置页面 调用，紧接在我们的UOptionsDataRegistry对象创建之后
+	void InitOptionsDataRegistry(ULocalPlayer* InOwningLocalPlayer);
+
+	const TArray<UListDataObject_Collection*>& GetRegisteredOptionsTabCollections() const { return RegisteredOptionsTabCollections; }
+
+
+private:
+	void InitGameplayCollectionTab();		// 游戏玩法
+	void InitControlsCollectionTab();		// 控制
+	void InitAudioCollectionTab();			// 音频
+	void InitVideoCollectionTab();			// 视频
+
+	UPROPERTY(Transient)
+	TArray<UListDataObject_Collection*> RegisteredOptionsTabCollections;
+	
+};
