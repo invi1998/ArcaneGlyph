282c27088a35be2f9e726eaa2aab520d90cc0a22
invi1998 <2267661826@qq.com>
2025-06-15 03:31:57 +0800

    可用选项字符串数组

--- Source/ArcaneGlyph/Public/Widget/Options/DataObject/ListDataObject_String.h
+++ Source/ArcaneGlyph/Public/Widget/Options/DataObject/ListDataObject_String.h
@@ -13,4 +13,8 @@ UCLASS()
 class ARCANEGLYPH_API UListDataObject_String : public UListDataObject_Value
 {
 	GENERATED_BODY()
+
+protected:
+	TArray<FString> AvailableOptionsStringArray;	// 可用选项字符串数组
+	TArray<FString> AvailableValuesStringArray;
 };
