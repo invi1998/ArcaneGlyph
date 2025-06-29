b0569847fc6b5219d916a3dbd40aebc51e6e5316
invi1998 <2267661826@qq.com>
2025-06-29 12:41:17 +0800

    数据默认值

--- Source/ArcaneGlyph/Public/Widget/Options/DataObject/ListDataObject_Value.h
+++ Source/ArcaneGlyph/Public/Widget/Options/DataObject/ListDataObject_Value.h
@@ -19,13 +19,25 @@ public:
 	void SetDataDynamicGetter(const TSharedPtr<FOptionsDataInteractionHelper>& InDataDynamicGetter);
 	void SetDataDynamicSetter(const TSharedPtr<FOptionsDataInteractionHelper>& InDataDynamicSetter);
 
+	void SetDefaultValueFromString(const FString& InDefaultValue) { DefaultStringValue = InDefaultValue; }
+
+	// **** Getter and Setter for DefaultStringValue **** //
+	virtual bool HasDefaultValue() const override { return DefaultStringValue.IsSet(); }
+	virtual bool CanResetToDefault() const override { return DefaultStringValue.IsSet(); }
+	
+
 protected:
 	TSharedPtr<FOptionsDataInteractionHelper> DataDynamicGetter;
 	TSharedPtr<FOptionsDataInteractionHelper> DataDynamicSetter;
 
+	FString GetDefaultValueAsString() const
+	{
+		return DefaultStringValue.IsSet() ? DefaultStringValue.GetValue() : FString();
+	}
+
 
 private:
 
-	TOptional<FString> DefaultStringValue;
+	TOptional<FString> DefaultStringValue;		// 默认字符串值，如果该值不为空，则表示该数据对象有默认值可供重置
 	
 };
