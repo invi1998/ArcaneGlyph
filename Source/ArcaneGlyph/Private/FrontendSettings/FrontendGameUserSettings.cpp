802fe41cd279e5d605a33997fb4a3ff44b510e43
invi1998 <2267661826@qq.com>
2025-06-30 18:01:26 +0800

    在 FrontendGameUserSettings 的设置函数中，每当音量值发生变化时通过 UGameplayStatics 中的 SetSoundMixClassOverride 和 PushSoundMixModifier 函数调控音量

--- Source/ArcaneGlyph/Private/FrontendSettings/FrontendGameUserSettings.cpp
+++ Source/ArcaneGlyph/Private/FrontendSettings/FrontendGameUserSettings.cpp
@@ -3,6 +3,11 @@
 
 #include "FrontendSettings/FrontendGameUserSettings.h"
 
+#include "FrontendSettings/FrontendDeveloperSettings.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundClass.h"
+#include "Sound/SoundMix.h"
+
 UFrontendGameUserSettings::UFrontendGameUserSettings()
 	: MasterVolume(1.f)
 {
@@ -25,6 +30,42 @@ void UFrontendGameUserSettings::SetCurrentGameplayTutorialModeEnabled(const FStr
 
 void UFrontendGameUserSettings::SetMasterVolume(float InMasterVolume)
 {
+	UWorld* InAudioWorld = nullptr;
+	const UFrontendDeveloperSettings* FrontendSettings = GetDefault<UFrontendDeveloperSettings>();
+
+	if (GEngine)
+	{
+		InAudioWorld = GEngine->GetCurrentPlayWorld();
+	}
+
+	if (!InAudioWorld || !FrontendSettings)
+	{
+		return; // 如果没有音频世界或前端设置，则直接返回
+	}
+
+	USoundClass* MasterSoundClass = nullptr;
+	if (UObject* MasterSoundClassObject = FrontendSettings->MasterSoundClassPath.TryLoad())
+	{
+		MasterSoundClass = Cast<USoundClass>(MasterSoundClassObject);
+	}
+
+	USoundMix* MasterSoundMix = nullptr;
+	if (UObject* MasterSoundMixObject = FrontendSettings->DefaultSoundMixPath.TryLoad())
+	{
+		MasterSoundMix = Cast<USoundMix>(MasterSoundMixObject);
+	}
+
 	MasterVolume = InMasterVolume;
+
+	UGameplayStatics::SetSoundMixClassOverride(
+		InAudioWorld,
+		MasterSoundMix,
+		MasterSoundClass,
+		InMasterVolume,
+		1.f, // 音量衰减
+		0.2f  // 混响衰减
+	);
+
+	UGameplayStatics::PushSoundMixModifier(InAudioWorld, MasterSoundMix);
 
 }
