2a931b75d3790a976e41dfdf3cce8c277b4efdbf
invi1998 <2267661826@qq.com>
2025-06-30 17:53:37 +0800

    设置音乐和音效类

--- Source/ArcaneGlyph/Public/FrontendSettings/FrontendDeveloperSettings.h
+++ Source/ArcaneGlyph/Public/FrontendSettings/FrontendDeveloperSettings.h
@@ -24,4 +24,25 @@ public:
 	UPROPERTY(Config, EditAnywhere, Category="Options Image References", meta=(Categories = "Frontend.Image", ForceInlineRow))
 	TMap<FGameplayTag, TSoftObjectPtr<UTexture2D>> FrontendOptionsSoftImageMap;	// 前端选项图片映射
 
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath MasterSoundClassPath;	// 主音效类路径，用于设置主音量
+
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath MusicSoundClassPath;	// 音乐音效类路径，用于设置音乐音量
+
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath SFXSoundClassPath;	// 音效音效类路径，用于设置音效音量
+
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath UserInterfaceSoundClassPath;	// 用户界面音效类路径，用于设置用户界面音量
+	
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath InGameMusicSoundClassPath;	// 游戏内音乐音效类路径，用于设置游戏内音乐音量
+
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundClass", ForceInlineRow))
+	FSoftObjectPath MenuMusicSoundClassPath;	// 菜单音乐音效类路径，用于设置菜单音乐音量
+
+	UPROPERTY(Config, EditAnywhere, Category="Audio Settings", meta=(AllowedClasses = "/Script/Engine.SoundMix", ForceInlineRow))
+	FSoftObjectPath DefaultSoundMixPath;	// 默认音效混合路径，用于设置默认音效混合
+	
 };
