f92f6442c4e25f374fd9d5b247dba6f1626add55
invi1998 <2267661826@qq.com>
2025-07-14 11:09:06 +0800

    加载界面设置

---
+++ Source/ArcaneGlyph/Private/FrontendSettings/FrontendLoadingScreenSettings.cpp
@@ -0,0 +1,12 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "FrontendSettings/FrontendLoadingScreenSettings.h"
+
+#include "Blueprint/UserWidget.h"
+
+TSubclassOf<UUserWidget> UFrontendLoadingScreenSettings::GetLoadingScreenWidgetClassChecked() const
+{
+	checkf(!SoftLoadingScreenWidgetClass.IsNull(), TEXT("Loading Screen Widget Class is not set in FrontendLoadingScreenSettings! Please set it in the project settings."));
+	return SoftLoadingScreenWidgetClass.LoadSynchronous();
+}
---
+++ Source/ArcaneGlyph/Public/FrontendSettings/FrontendLoadingScreenSettings.h
@@ -0,0 +1,31 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "FrontendLoadingScreenSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = Game, DefaultConfig)
+class ARCANEGLYPH_API UFrontendLoadingScreenSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	TSubclassOf<UUserWidget> GetLoadingScreenWidgetClassChecked() const;
+	
+	UPROPERTY(Config, EditAnywhere, Category="Loading Screen Settings")
+	TSoftClassPtr<UUserWidget> SoftLoadingScreenWidgetClass;	// 异步加载的加载屏幕小部件类
+
+	UPROPERTY(Config, EditAnywhere, Category="Loading Screen Settings")
+	float HoldLoadingScreenExtraSeconds = 5.0f;	// 在加载完成后额外保持加载屏幕的时间（秒）
+
+	UPROPERTY(Config, EditAnywhere, Category="Loading Screen Settings")
+	bool bShouldLoadingScreenInEditor = false;	// 是否在编辑器中显示加载屏幕
+
+	
+	
+};
