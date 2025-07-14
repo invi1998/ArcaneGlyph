741b90dd2d85e0cc0898eab36ca57cb67737d56a
invi1998 <2267661826@qq.com>
2025-07-14 17:41:11 +0800

    加载界面子系统

---
+++ Source/ArcaneGlyph/Private/Subsystems/FrontendLoadingScreenSubsystem.cpp
@@ -0,0 +1,48 @@
+﻿// INVI_1998 All Rights Reserved.
+
+
+#include "Subsystems/FrontendLoadingScreenSubsystem.h"
+
+bool UFrontendLoadingScreenSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		// 如果不是专用服务器实例，就查找是否有派生类，如果没有派生类，则不创建子系统
+		TArray<UClass*> FoundClasses;
+		GetDerivedClasses(GetClass(), FoundClasses);
+
+		return FoundClasses.IsEmpty();
+	}
+
+	// 如果是专用服务器实例，则不创建子系统（因为服务器不需要UI）
+	return false;
+}
+
+void UFrontendLoadingScreenSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	// 绑定地图预加载事件
+	FCoreUObjectDelegates::PreLoadMapWithContext.AddUObject(this, &ThisClass::OnMapPreLoaded);
+
+	// 绑定地图加载完成事件
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnMapPostLoaded);
+}
+
+void UFrontendLoadingScreenSubsystem::Deinitialize()
+{
+	// 解绑地图预加载事件
+	FCoreUObjectDelegates::PreLoadMapWithContext.RemoveAll(this);
+
+	// 解绑地图加载完成事件
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+	
+	Super::Deinitialize();
+}
+
+void UFrontendLoadingScreenSubsystem::OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName)
+{
+	
+}
+
+void UFrontendLoadingScreenSubsystem::OnMapPostLoaded(UWorld* LoadedWorld)
+{
+}
---
+++ Source/ArcaneGlyph/Public/Subsystems/FrontendLoadingScreenSubsystem.h
@@ -0,0 +1,28 @@
+﻿// INVI_1998 All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FrontendLoadingScreenSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANEGLYPH_API UFrontendLoadingScreenSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	// begin USubsystem Interface
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	// end USubsystem Interface
+
+private:
+	void OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName);
+	void OnMapPostLoaded(UWorld* LoadedWorld);
+	
+};
